1f1b51560522fd342517b9aa5706c79bddf0d826
lilylilyngo <13605973@student.uts.edu.au>
2021-04-25 17:06:20 +1000

    Correlation coefficent

--- main.py
+++ main.py
@@ -12,21 +12,22 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv(r'/Users/lilyngo/Downloads/BTC_USD_2020-04-19_2021-04-18-CoinDesk.csv',header=0,index_col=1,parse_dates=[0]) # rename to your path
-df.plot(y=['Closing Price (USD)','24h Open (USD)','24h High (USD)','24h Low (USD)'])
-plt.show()
+#df = pd.read_csv(r'/Users/lilyngo/Downloads/BTC_USD_2020-04-19_2021-04-18-CoinDesk.csv',header=0,index_col=1,parse_dates=[0]) # rename to your path
+#df.plot(y=['Closing Price (USD)','24h Open (USD)','24h High (USD)','24h Low (USD)'])
+#plt.show()
 
 
 data = pd.read_csv(r'/Users/lilyngo/Downloads/BTC_USD_2020-04-19_2021-04-18-CoinDesk.csv') # rename to your path
+data_two = pd.read_csv(r'/Users/lilyngo/Downloads/Excelrates.csv')
 x = data['Closing Price (USD)']
-y = data['24h High (USD)']
+y = data_two['USD']
 
 print(np.corrcoef(x, y))
 
 plt.scatter(x, y)
 plt.title('A plot to show the correlation ')
 plt.xlabel('Closing Price (USD)')
-plt.ylabel('24h High (USD)')
+plt.ylabel(' (USD)')
 plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), color='yellow')
 plt.show()
 
